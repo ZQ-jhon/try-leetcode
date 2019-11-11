bd371ed1acd075d4bf2cb967b213f7f59f57010c
zhangqiang <350037310@qq.com>
2019-11-11 23:05:05 +0800

    feat: :sparkles: adding two array intersect question

---
+++ pre-case/Array/06-intersect.ts
@@ -0,0 +1,40 @@
+import { TimeStatistics } from "../../utils/time-log";
+const TEST_CASE1 = [[1, 2, 2, 3, 4, 5, 7, 8, 9], [2, 4, 8, 9]];
+// 给定两个数组，编写一个函数来计算它们的交集。
+// 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+// 输出: [2,2]
+
+// 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// 输出: [4,9]
+
+/**
+ * 读了半天题目，没读懂
+ * 
+ * [1,2,3,4,4,4,4,4,4,4,5,6,7,8,9]
+ * [7,4,8,5,9]
+ * 结果不应该是 [7,4,5,8,9] 吗？4 明明在两个数组出出现次数不一样啊? 难道是求最大有序交集？
+ * 
+ * 后来看明白了，求的是无序交集
+ * 思路还是 map，降低复杂度
+ */
+
+/**
+ * O(T) = O(N+N) = O(N)
+ */
+function intersect(arr1: number[], arr2: number[]): number[] {
+    const map = new Map<number, number>();
+    const result: number[] = [];
+    // 建立索引
+    arr1.forEach(v => map.has(v) ? map.set(v, map.get(v) as number + 1) : map.set(v, 1));
+    // 每次有相同的，就 push，并抹去一次对应的 duplicate record
+    arr2.forEach(v => {
+        if (map.get(v) as number > 0) {
+            map.set(v, map.get(v) as number - 1);
+            result.push(v);
+        }
+    });
+    return result;
+}
+
+const t = new TimeStatistics(intersect, TEST_CASE1); // 3.6 ms
+t.executeTenThousandTimes(); // 7.7 ms
