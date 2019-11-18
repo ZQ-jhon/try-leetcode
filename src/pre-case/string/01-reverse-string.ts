647db1908061fdc7d8b6a8ae32e7999f796c7750
zhangqiang <350037310@qq.com>
2019-11-18 22:04:23 +0800

    fix: :bug: fix for string question one

--- src/pre-case/string/01-reverse-string.ts
+++ src/pre-case/string/01-reverse-string.ts
@@ -1,39 +1,38 @@
-// 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+import { TimeStatistics } from "../../../utils/time-log";
 
-// 示例 1:
+const TEST_CASE1 = ["h","e","l","l","o"];
 
-// 输入: 123
-// 输出: 321
-//  示例 2:
+// 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
 
-// 输入: -123
-// 输出: -321
+// 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+
+// 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+
+ 
+
+// 示例 1：
+
+// 输入：["h","e","l","l","o"]
+// 输出：["o","l","l","e","h"]
 
 /**
- * 不考虑溢出，只是简单替换，可以这么写
+ * 禁止使用 reverse() 
  */
-function reverseInteger1(x: number) {
-    x = x < 0 ? -x : x;
-    const arr = String(x).split('');
-    let i = 0, j = arr.length - 1;
+
+/**
+ * 双指针对撞
+ * O(T) = O(1)
+ */
+
+function reverseString(s: string[]) {
+    let i = 0, j = s.length - 1;
     while (i < j) {
-        [arr[i], arr[j]] = [arr[j], arr[i]];
+        [s[i], s[j]] = [s[j], s[i]];
         i++;
         j--;
     }
-    if(x < 0) {
-        arr.unshift('-');
-    }
-    console.log(arr);
-    arr.join('');
-    return +arr;
-}
+    return s;
+};
 
-
-
-/**
- * // TODO: 再加上溢出判断
- */
-function reverseInteger(x: number) { 
-
-}
+const t = new TimeStatistics(reverseString, [TEST_CASE1]);
+t.executeTenThousandTimes();
